6be853204caf46c6adf9723d417d0372c93c91c6
ishah <syishah.gillani.123@gmail.com>
2025-08-05 16:33:15 +0500

    array ascending sorting

---
+++ arraysortingascending.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int array[10],size,temp;
+	cout<<"enter the size of the array"<<endl;
+	cin>>size;
+	cout<<"enter the elements in the array:"<<endl;
+	for(int i=0;i<size;i++){
+	cin>>array[i];	
+	}
+	for(int i=0;i<size;i++){
+		for(int j=i+1;j<size;j++){
+			if(array[i]>array[j]){
+			temp=array[i];
+			array[i]=array[j];
+			array[j]=temp;	
+			}
+		}
+	}
+	for(int i=0;i<size;i++){
+		cout<<"the elements in array is:"<<array[i]<<endl;
+	}
+	return 0;
+}
